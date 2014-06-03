95e1ee0a7209f83b79e091e8b981d4461f3acb4c
ArjunMurthy <arjun_na@live.com>
2014-06-03 16:44:02 -0700

    Changed
    i--> I

---
+++ helloworld/src/helloworld.c
@@ -0,0 +1,23 @@
+/*
+ ============================================================================
+ Name        : helloworld.c
+ Author      : ArjunMurthy
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	int i;
+	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+
+	printf("\n finally I accomplished");
+
+	scanf("%d",&i);
+	printf("\n %d",i);
+	return EXIT_SUCCESS;
+}
